4dd677511c4245869e87fa8bcca15e5141613896
John Morley <john.morley@bjss.com>
2019-02-12 13:27:37 +0000

    Add date windowing to NTA query

--- journal-poller/src/functions/pollJournals/framework/repo/mysql/non-test-activity-repository.ts
+++ journal-poller/src/functions/pollJournals/framework/repo/mysql/non-test-activity-repository.ts
@@ -1,9 +1,13 @@
 import * as mysql from 'mysql';
 import { blockingQuery } from '../database';
 import { ExaminerNonTestActivity } from '../../../domain/examiner-non-test-activity';
+import * as moment from 'moment';
 
 export const getNonTestActivities = async (connectionPool: mysql.Pool)
   : Promise<ExaminerNonTestActivity[]> => {
+  const sqlYearFormat = 'YYYY-MM-DD';
+  const windowStart = moment().format(sqlYearFormat);
+  const windowEnd = moment().add(3, 'days').format(sqlYearFormat);
   const res = await blockingQuery(
     connectionPool,
     `
@@ -13,9 +17,10 @@ export const getNonTestActivities = async (connectionPool: mysql.Pool)
     join NON_TEST_ACTIVITY_REASON reason on w.non_test_activity_code = reason.non_test_activity_code
     left join TEST_CENTRE tc on w.tc_id = tc.tc_id
     left join TEST_CENTRE_NAME tcn on w.tc_id = tcn.tc_id
-    where w.programme_date between '2017-08-03' and '2017-08-06'
-    and w.examiner_end_date >= '2017-08-03'
+    where w.programme_date between ? and ?
+    and w.examiner_end_date >= ?
     `,
+    [windowStart, windowEnd, windowStart],
   );
   return res.map(mapRow);
 };
