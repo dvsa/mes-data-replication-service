55737ba249b8a42abe21f656b992cbe44c96f4ad
John Morley <john.morley@bjss.com>
2019-02-12 14:04:59 +0000

    Calculate date bounds on advance test slots query

--- journal-poller/src/functions/pollJournals/framework/repo/mysql/advance-test-slots-repository.ts
+++ journal-poller/src/functions/pollJournals/framework/repo/mysql/advance-test-slots-repository.ts
@@ -1,22 +1,28 @@
 import * as mysql from 'mysql';
 import { blockingQuery } from '../database';
 import { ExaminerAdvanceTestSlot } from '../../../domain/examiner-advance-test-slot';
+import * as moment from 'moment';
 
 export const getAdvanceTestSlots = async (connectionPool: mysql.Pool):
   Promise<ExaminerAdvanceTestSlot[]> => {
+  const sqlYearFormat = 'YYYY-MM-DD';
+  const windowStart = moment().add(4, 'days').format(sqlYearFormat);
+  const windowEnd = moment().add(13, 'days').format(sqlYearFormat);
+
   const res =  await blockingQuery(
     connectionPool,
+    /* tslint:disable */
     `
-    select w.individual_id, w.slot_id, w.start_time, w.minutes, w.tc_id, tcn.tc_name,
-    tc.tc_cost_centre_code, vst.short_vst_desc
+    select w.individual_id, w.slot_id, w.start_time, w.minutes, w.tc_id, tcn.tc_name, tc.tc_cost_centre_code, vst.short_vst_desc
     from WORK_SCHEDULE_SLOTS w
-    join TEST_CENTRE tc on w.tc_id = tc.tc_id
-    join TEST_CENTRE_NAME tcn on w.tc_id = tcn.tc_id
-    join VEHICLE_SLOT_TYPE vst on w.vst_code = vst.vst_code
-    where w.programme_date between STR_TO_DATE('03/08/2017', '%d/%m/%Y')
-    and STR_TO_DATE('16/08/2017', '%d/%m/%Y')
-    and w.examiner_end_date > STR_TO_DATE('03/08/2017', '%d/%m/%Y')
+        join TEST_CENTRE tc on w.tc_id = tc.tc_id
+        join TEST_CENTRE_NAME tcn on w.tc_id = tcn.tc_id
+        join VEHICLE_SLOT_TYPE vst on w.vst_code = vst.vst_code
+    where w.programme_date between STR_TO_DATE(?, '%d/%m/%Y') and STR_TO_DATE(?, '%d/%m/%Y')
+    and w.examiner_end_date > STR_TO_DATE(?, '%d/%m/%Y')
     `,
+    [windowStart, windowEnd, windowStart],
+    /* tslint:enable */
   );
   return res.map(mapRow);
 };
