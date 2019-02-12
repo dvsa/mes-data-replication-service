5f24925091250f79760d72d7862e2e1501f4601e
John Morley <john.morley@bjss.com>
2019-02-12 13:29:40 +0000

    Use latest personal commitment query

--- journal-poller/src/functions/pollJournals/framework/repo/mysql/personal-commitment-repository.ts
+++ journal-poller/src/functions/pollJournals/framework/repo/mysql/personal-commitment-repository.ts
@@ -6,28 +6,25 @@ export const getPersonalCommitments = async (connectionPool: mysql.Pool)
   : Promise<ExaminerPersonalCommitment[]> => {
   const res = await blockingQuery(
     connectionPool,
+    /* tslint:disable */
     `
-    select e.individual_id, pc.commitment_id, pc.start_date_time, pc.end_date_time,
-    pc.non_test_activity_code, reason.reason_desc
-    from EXAMINER e
-    join PERSONAL_COMMITMENT pc on e.individual_id = pc.individual_id
-    join NON_TEST_ACTIVITY_REASON reason
-    on pc.non_test_activity_code = reason.non_test_activity_code
+    select e.individual_id, pc.commitment_id, pc.start_date_time, pc.end_date_time, pc.non_test_activity_code, reason.reason_desc
+    from EXAMINER e 
+        join PERSONAL_COMMITMENT pc on e.individual_id = pc.individual_id
+        join NON_TEST_ACTIVITY_REASON reason on pc.non_test_activity_code = reason.non_test_activity_code
     where (
-      DATE(pc.start_date_time) between STR_TO_DATE('03/08/2017', '%d/%m/%Y')
-      and STR_TO_DATE('16/08/2017', '%d/%m/%Y')
-      or DATE(pc.end_date_time) between STR_TO_DATE('03/08/2017', '%d/%m/%Y')
-      and STR_TO_DATE('16/08/2017', '%d/%m/%Y')
+        DATE(pc.start_date_time) between STR_TO_DATE('03/08/2017', '%d/%m/%Y') and STR_TO_DATE('16/08/2017', '%d/%m/%Y')
+        or DATE(pc.end_date_time) between STR_TO_DATE('03/08/2017', '%d/%m/%Y') and STR_TO_DATE('16/08/2017', '%d/%m/%Y')
     )
     and IFNULL(e.grade_code, 'ZZZ') != 'DELE'
     and exists (
-      select end_date
-      from EXAMINER_STATUS es
-      where es.individual_id = e.individual_id
-      and IFNULL(es.end_date, STR_TO_DATE('01/01/4000', '%d/%m/%Y'))
-      > STR_TO_DATE('03/08/2017', '%d/%m/%Y')
+        select end_date 
+        from EXAMINER_STATUS es
+        where es.individual_id = e.individual_id
+        and IFNULL(es.end_date, STR_TO_DATE('01/01/4000', '%d/%m/%Y')) > STR_TO_DATE('03/08/2017', '%d/%m/%Y')
     )
     `,
+    /* tslint:enable */
   ) as PersonalCommitmentRow[];
   return res.map(mapRow);
 };
