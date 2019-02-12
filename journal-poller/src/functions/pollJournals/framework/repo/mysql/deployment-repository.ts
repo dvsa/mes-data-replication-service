0568c33bb052d51ecef915127ca36bffe1e7e08a
John Morley <john.morley@bjss.com>
2019-02-12 14:10:17 +0000

    Calculate date bounds for deployment query

--- journal-poller/src/functions/pollJournals/framework/repo/mysql/deployment-repository.ts
+++ journal-poller/src/functions/pollJournals/framework/repo/mysql/deployment-repository.ts
@@ -1,37 +1,39 @@
 import * as mysql from 'mysql';
 import { blockingQuery } from '../database';
 import { ExaminerDeployment } from '../../../domain/examiner-deployment';
+import * as moment from 'moment';
 
 export const getDeployments = async (connectionPool: mysql.Pool): Promise<ExaminerDeployment[]> => {
+  const sqlYearFormat = 'YYYY-MM-DD';
+  const windowStart = moment().format(sqlYearFormat);
+  const windowEnd = moment().add(6, 'months').format(sqlYearFormat);
+
   const res = await blockingQuery(
     connectionPool,
+    /* tslint:disable */
     `
-    select d.deployment_id, e.individual_id, d.tc_id, tcn.tc_name,
-    tc.tc_cost_centre_code, p.programme_date
+    select d.deployment_id, e.individual_id, d.tc_id, tcn.tc_name, tc.tc_cost_centre_code, p.programme_date
     from EXAMINER e
-      join DEPLOYMENT d on e.individual_id = d.individual_id
-      join TEST_CENTRE tc on d.tc_id = tc.tc_id
-      join TEST_CENTRE_NAME tcn on d.tc_id = tcn.tc_id
-      join PROGRAMME p on p.individual_id = e.individual_id
+        join DEPLOYMENT d on e.individual_id = d.individual_id
+        join TEST_CENTRE tc on d.tc_id = tc.tc_id
+        join TEST_CENTRE_NAME tcn on d.tc_id = tcn.tc_id
+        join PROGRAMME p on p.individual_id = e.individual_id
     where (
-      DATE(d.start_date) between
-        STR_TO_DATE('03/08/2017', '%d/%m/%Y')
-        and STR_TO_DATE('02/02/2018', '%d/%m/%Y')
-      or DATE(d.end_date) between
-        STR_TO_DATE('03/08/2017', '%d/%m/%Y')
-        and STR_TO_DATE('02/02/2018', '%d/%m/%Y')
+        DATE(d.start_date) between STR_TO_DATE(?, '%d/%m/%Y') and STR_TO_DATE(?, '%d/%m/%Y')
+        or DATE(d.end_date) between STR_TO_DATE(?, '%d/%m/%Y') and STR_TO_DATE(?, '%d/%m/%Y')
     )
     and DATE(p.programme_date) between DATE(d.start_date) and DATE(d.end_date)
     and p.tc_id = d.tc_id
     and IFNULL(e.grade_code, 'ZZZ') != 'DELE'
     and exists (
-      select end_date
-      from EXAMINER_STATUS es
-      where es.individual_id = e.individual_id
-      and IFNULL(es.end_date, STR_TO_DATE('01/01/4000', '%d/%m/%Y'))
-        > STR_TO_DATE('03/08/2017', '%d/%m/%Y')
+        select end_date
+        from EXAMINER_STATUS es
+        where es.individual_id = e.individual_id
+        and IFNULL(es.end_date, STR_TO_DATE('01/01/4000', '%d/%m/%Y')) > STR_TO_DATE(?, '%d/%m/%Y')
     )
     `,
+    /* tslint:enable */
+    [windowStart, windowEnd, windowStart, windowEnd, windowStart],
   );
   return res.map(mapRow);
 };
