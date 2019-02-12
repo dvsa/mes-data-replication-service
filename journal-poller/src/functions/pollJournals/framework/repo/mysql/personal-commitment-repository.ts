a451f757307a22f6527b03d5e01a566866b6030c
John Morley <john.morley@bjss.com>
2019-02-12 13:33:13 +0000

    Parameterise date window for personal commitment query

--- journal-poller/src/functions/pollJournals/framework/repo/mysql/personal-commitment-repository.ts
+++ journal-poller/src/functions/pollJournals/framework/repo/mysql/personal-commitment-repository.ts
@@ -1,9 +1,14 @@
 import * as mysql from 'mysql';
 import { blockingQuery } from '../database';
 import { ExaminerPersonalCommitment } from '../../../domain/examiner-personal-commitment';
+import * as moment from 'moment';
 
 export const getPersonalCommitments = async (connectionPool: mysql.Pool)
   : Promise<ExaminerPersonalCommitment[]> => {
+  const sqlYearFormat = 'YYYY-MM-DD';
+  const windowStart = moment().format(sqlYearFormat);
+  const windowEnd = moment().add(13, 'days').format(sqlYearFormat);
+
   const res = await blockingQuery(
     connectionPool,
     /* tslint:disable */
@@ -13,17 +18,18 @@ export const getPersonalCommitments = async (connectionPool: mysql.Pool)
         join PERSONAL_COMMITMENT pc on e.individual_id = pc.individual_id
         join NON_TEST_ACTIVITY_REASON reason on pc.non_test_activity_code = reason.non_test_activity_code
     where (
-        DATE(pc.start_date_time) between STR_TO_DATE('03/08/2017', '%d/%m/%Y') and STR_TO_DATE('16/08/2017', '%d/%m/%Y')
-        or DATE(pc.end_date_time) between STR_TO_DATE('03/08/2017', '%d/%m/%Y') and STR_TO_DATE('16/08/2017', '%d/%m/%Y')
+        DATE(pc.start_date_time) between STR_TO_DATE(?, '%d/%m/%Y') and STR_TO_DATE(?, '%d/%m/%Y')
+        or DATE(pc.end_date_time) between STR_TO_DATE(?, '%d/%m/%Y') and STR_TO_DATE(?, '%d/%m/%Y')
     )
     and IFNULL(e.grade_code, 'ZZZ') != 'DELE'
     and exists (
         select end_date 
         from EXAMINER_STATUS es
         where es.individual_id = e.individual_id
-        and IFNULL(es.end_date, STR_TO_DATE('01/01/4000', '%d/%m/%Y')) > STR_TO_DATE('03/08/2017', '%d/%m/%Y')
+        and IFNULL(es.end_date, STR_TO_DATE('01/01/4000', '%d/%m/%Y')) > STR_TO_DATE(?, '%d/%m/%Y')
     )
     `,
+    [windowStart, windowEnd, windowStart, windowEnd, windowStart]
     /* tslint:enable */
   ) as PersonalCommitmentRow[];
   return res.map(mapRow);
