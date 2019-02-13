5d99476095f3624d883ed2da90839c2577d158e9
John Morley <john.morley@bjss.com>
2019-02-13 09:44:47 +0000

    Ensure we query examiners who are active on todays date

--- journal-poller/src/functions/pollJournals/framework/repo/mysql/examiner-repository.ts
+++ journal-poller/src/functions/pollJournals/framework/repo/mysql/examiner-repository.ts
@@ -1,7 +1,11 @@
 import * as mysql from 'mysql';
 import { blockingQuery } from '../database';
+import * as moment from 'moment';
 
 export const getExaminers = async (connectionPool: mysql.Pool): Promise<any[]> => {
+  const sqlYearFormat = 'YYYY-MM-DD';
+  const windowStart = moment().format(sqlYearFormat);
+
   const res = await blockingQuery(
     connectionPool,
     `
@@ -9,15 +13,13 @@ export const getExaminers = async (connectionPool: mysql.Pool): Promise<any[]> =
         i.first_forename as first_forename, i.second_forename as second_forename,
         i.third_forename as third_forename, i.family_name as family_name
     from INDIVIDUAL i
-    left join REF_DATA_ITEM_MASTER title_ref on i.title_code = title_ref.item_id
-    join EXAMINER e on e.individual_id = i.individual_id
-    where IFNULL(e.grade_code, 'ZZZ') != 'DELE'
-    and exists (
-    select end_date from EXAMINER_STATUS es
-    where es.individual_id = e.individual_id
-    and IFNULL(es.end_date, STR_TO_DATE('01/01/4000', '%d/%m/%Y'))
-      > STR_TO_DATE('14/08/2017', '%d/%m/%Y')
-    )
-  `);
+        join EXAMINER e on e.individual_id = i.individual_id
+        left join EXAMINER_STATUS es on es.individual_id = e.individual_id
+        left join REF_DATA_ITEM_MASTER title_ref on i.title_code = title_ref.item_id
+    where IFNULL(e.grade_code, 'ZZZ') <> 'DELE'
+    and IFNULL(es.end_date, '4000-01-01') >= ?
+    `,
+    [windowStart],
+  );
   return res;
 };
