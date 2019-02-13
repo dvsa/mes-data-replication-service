5d80a574f55780661004fa641507aa0b3c852885
John Morley <john.morley@bjss.com>
2019-02-13 11:02:07 +0000

    Refactor journal enrichment with new datasets to eliminate duplication

--- journal-poller/src/functions/pollJournals/application/transformer.ts
+++ journal-poller/src/functions/pollJournals/application/transformer.ts
@@ -1,62 +1,68 @@
 import { AllDatasets } from '../framework/handler';
-import { groupBy } from 'lodash';
-import { ExaminerWorkSchedule } from '../../../common/domain/Schema';
+import { groupBy, get } from 'lodash';
+import { ExaminerWorkSchedule, PersonalCommitment } from '../../../common/domain/Schema';
 import { JournalWrapper } from '../domain/journal-wrapper';
 import * as crypto from 'crypto';
+import { ExaminerNonTestActivity } from '../domain/examiner-non-test-activity';
+import { ExaminerAdvanceTestSlot } from '../domain/examiner-advance-test-slot';
+import { ExaminerDeployment } from '../domain/examiner-deployment';
+import { ExaminerTestSlot } from '../domain/examiner-test-slot';
+import { ExaminerPersonalCommitment } from '../domain/examiner-personal-commitment';
 
 export const transform = (examiners: any[], datasets: AllDatasets): JournalWrapper[] => {
-  const advanceTestSlotByExaminer = groupBy(datasets.advanceTestSlots, ats => ats.examinerId);
+  const testSlotsByExaminer = groupBy(datasets.testSlots, test => test.examinerId);
+  const advanceTestsByExaminer = groupBy(datasets.advanceTestSlots, ats => ats.examinerId);
   const deploymentsByExaminer = groupBy(datasets.deployments, deployment => deployment.examinerId);
-  const nonTestActivitiesByExaminer = groupBy(
+  const nonTestActByExaminer = groupBy(
     datasets.nonTestActivities,
     nonTestActivity => nonTestActivity.examinerId,
   );
-  const personalCommitmentsByExaminer = groupBy(
+  const commitmentsByExaminer = groupBy(
     datasets.personalCommitments,
     personalCommitment => personalCommitment.examinerId,
   );
-  const testSlotsByExaminer = groupBy(
-    datasets.testSlots,
-    test => test.examinerId,
-  );
 
   const journals: JournalWrapper[] = examiners.map((examiner) => {
-    const individualId = examiner.individual_id.toString();
+    const indId = examiner.individual_id.toString();
     const staffNumber = examiner.staff_number.toString();
-    const journal: ExaminerWorkSchedule = {
+    let journal: ExaminerWorkSchedule = {
       examiner: {
         staffNumber,
       },
     };
 
-    if (testSlotsByExaminer[individualId]) {
-      journal.testSlots = testSlotsByExaminer[individualId]
-        .map(examinerTestSlot => examinerTestSlot.testSlot);
-    }
+    journal = enrichJournalWithDataset(journal, indId, testSlotsByExaminer, 'testSlot');
+    journal = enrichJournalWithDataset(journal, indId, commitmentsByExaminer, 'personalCommitment');
+    journal = enrichJournalWithDataset(journal, indId, nonTestActByExaminer, 'nonTestActivity');
+    journal = enrichJournalWithDataset(journal, indId, advanceTestsByExaminer, 'advanceTestSlot');
+    journal = enrichJournalWithDataset(journal, indId, deploymentsByExaminer, 'deployment');
 
-    if (personalCommitmentsByExaminer[individualId]) {
-      journal.personalCommitments = personalCommitmentsByExaminer[individualId]
-        .map(examinerPersonalCommitment => examinerPersonalCommitment.personalCommitment);
-    }
-
-    if (nonTestActivitiesByExaminer[individualId]) {
-      journal.nonTestActivities = nonTestActivitiesByExaminer[individualId]
-        .map(examinerNonTestActivity => examinerNonTestActivity.nonTestActivity);
-    }
-
-    if (advanceTestSlotByExaminer[individualId]) {
-      journal.advanceTestSlots = advanceTestSlotByExaminer[individualId]
-        .map(examinerAdvanceTestSlot => examinerAdvanceTestSlot.advanceTestSlot);
-    }
-
-    if (deploymentsByExaminer[individualId]) {
-      journal.deployments = deploymentsByExaminer[individualId]
-        .map(examinerDeployment => examinerDeployment.deployment);
-    }
     const hash = crypto.createHash('sha256').update(JSON.stringify(journal)).digest('hex');
     const lastUpdatedAt = Date.now();
     return { staffNumber, hash, lastUpdatedAt, journal };
   });
 
   return journals;
+};
+
+const enrichJournalWithDataset = (
+  journal: ExaminerWorkSchedule,
+  individualId: string,
+  dataset: { [key: string]: (
+    ExaminerTestSlot
+    | ExaminerPersonalCommitment
+    | ExaminerNonTestActivity
+    | ExaminerAdvanceTestSlot
+    | ExaminerDeployment
+  )[] },
+  datasetKey: string,
+): ExaminerWorkSchedule => {
+  let enrichedJournal = journal;
+  if (dataset[individualId]) {
+    enrichedJournal =  {
+      ...journal,
+      [datasetKey]: dataset[individualId].map(ds => get(ds, datasetKey)),
+    };
+  }
+  return enrichedJournal;
 };
