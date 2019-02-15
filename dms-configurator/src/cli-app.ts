a6f93abac6ff509126065aa857350aca157a0a91
John Morley <41190821+johnmorleybjss@users.noreply.github.com>
2019-02-15 11:41:56 +0000

    Fix lint issues in DMS configurator (#11)

--- dms-configurator/src/cli-app.ts
+++ dms-configurator/src/cli-app.ts
@@ -11,52 +11,50 @@ import { DateTime, Duration } from 'luxon';
 const logger = getLogger('cli-app', 'debug');
 const dms = new DmsApi('eu-west-1');
 
-
 /**
  * Adds source filters to the "datefiltered" dataset.
  * @param options - the options to add to
  */
 function addDateFilters(options: Options) {
-    const endDate =  startDate.plus(highLevelSlotTimeWindow);
-    addBetweenFilter(options, 'PROGRAMME', 'PROGRAMME_DATE', startDate, endDate);
-    addBetweenFilter(options, 'PROGRAMME_SLOT', 'PROGRAMME_DATE', startDate, endDate);
+  const endDate =  startDate.plus(highLevelSlotTimeWindow);
+  addBetweenFilter(options, 'PROGRAMME', 'PROGRAMME_DATE', startDate, endDate);
+  addBetweenFilter(options, 'PROGRAMME_SLOT', 'PROGRAMME_DATE', startDate, endDate);
     // all personal commitments that overlap with our time window of interest
-    const personalCommitmentEndDate = startDate.plus(highLevelSlotTimeWindow);
+  const personalCommitmentEndDate = startDate.plus(highLevelSlotTimeWindow);
     // As we're querying PersonalCommitment on DateTime, we need to include the whole day
-    const personalCommitmentEndDateTime = personalCommitmentEndDate.plus({ hours: 23, minutes: 59, seconds: 59 });
-    addOnOrBeforeFilter(options, 'PERSONAL_COMMITMENT', 'START_DATE_TIME', personalCommitmentEndDateTime); // i.e. start before or during
-    addOnOrAfterFilter(options, 'PERSONAL_COMMITMENT', 'END_DATE_TIME', startDate); // i.e. end during or after
+  const personalCommitmentEndDateTime = personalCommitmentEndDate.plus({ hours: 23, minutes: 59, seconds: 59 });
+  addOnOrBeforeFilter(options, 'PERSONAL_COMMITMENT', 'START_DATE_TIME', personalCommitmentEndDateTime); // i.e. start before or during
+  addOnOrAfterFilter(options, 'PERSONAL_COMMITMENT', 'END_DATE_TIME', startDate); // i.e. end during or after
     // all deployments that overlap with our time window of interest
-    const deploymentEndDate =  startDate.plus(deploymentTimeWindow);
-    addOnOrBeforeFilter(options, 'DEPLOYMENT', 'START_DATE', deploymentEndDate); // i.e. start before or during
-    addOnOrAfterFilter(options, 'DEPLOYMENT', 'END_DATE', startDate); // i.e. end during or after
+  const deploymentEndDate =  startDate.plus(deploymentTimeWindow);
+  addOnOrBeforeFilter(options, 'DEPLOYMENT', 'START_DATE', deploymentEndDate); // i.e. start before or during
+  addOnOrAfterFilter(options, 'DEPLOYMENT', 'END_DATE', startDate); // i.e. end during or after
 }
 
 /**
  * Creates (or updates) all DMS tasks.
  */
 async function createAllTasks(): Promise<void> {
-    try {
-        const sourceEndpointArn = await dms.getEndpointArn('mes-perf-dms-source');
-        logger.debug("source endpoint arn is %s", sourceEndpointArn);
+  try {
+    const sourceEndpointArn = await dms.getEndpointArn('mes-perf-dms-source');
+    logger.debug('source endpoint arn is %s', sourceEndpointArn);
 
-        const destEndpointArn = await dms.getEndpointArn('mes-perf-dms-target');
-        logger.debug("dest endpoint arn is %s", destEndpointArn);
+    const destEndpointArn = await dms.getEndpointArn('mes-perf-dms-target');
+    logger.debug('dest endpoint arn is %s', destEndpointArn);
 
-        const replicationInstanceArn = await dms.getReplicationInstanceArn('mes-perf-dms-replicator');
-        logger.debug("repl instance arn is %s", replicationInstanceArn);
+    const replicationInstanceArn = await dms.getReplicationInstanceArn('mes-perf-dms-replicator');
+    logger.debug('repl instance arn is %s', replicationInstanceArn);
 
-        await dms.createTask('static-full-load-and-cdc', '../table-mappings/static-tables.json',
+    await dms.createTask('static-full-load-and-cdc', '../table-mappings/static-tables.json',
                   replicationInstanceArn, sourceEndpointArn, destEndpointArn);
 
-        await dms.createTask('dateFiltered-full-load-and-cdc', '../table-mappings/dateFiltered-tables.json',
+    await dms.createTask('dateFiltered-full-load-and-cdc', '../table-mappings/dateFiltered-tables.json',
                   replicationInstanceArn, sourceEndpointArn, destEndpointArn, addDateFilters);
 
-    } catch (e) {
-        logger.error("Error creating DMS task: %s", e);
-    }
+  } catch (e) {
+    logger.error('Error creating DMS task: %s', e);
+  }
 }
-
 
 // pretend today is...
 const startDate = DateTime.fromISO('2017-08-03');
@@ -64,21 +62,21 @@ const startDate = DateTime.fromISO('2017-08-03');
 // time window to migrate detailed journal data (i.e. slots and bookings with candidate details etc)
 // used for the "Test Slots" and "Non Test Activities" datasets
 // => 4 elaspsed days (today plus 3 further days) is the maximum amount (e.g. on thursday - fri/sat/sun/mon)
-const detailedSlotTimeWindow = Duration.fromObject({ days: 3});
+const detailedSlotTimeWindow = Duration.fromObject({ days: 3 });
 
-// time window to migrate high level journal data (i.e. slots without and booking/candidate details etc) 
+// time window to migrate high level journal data (i.e. slots without and booking/candidate details etc)
 // used for the "Personal Commitments" and "Advanced Test Slots" datasets
 // => 14 elapsed days (today plus 13 further days)
-const highLevelSlotTimeWindow = Duration.fromObject({ days: 13}); 
+const highLevelSlotTimeWindow = Duration.fromObject({ days: 13 });
 
 // time window to migrate deployments (i.e. notice of being deployed out to another test centre)
 // used for the "Deployments" dataset
 // => 6 months elapsed days of deployments (including today)
 const deploymentTimeWindow = Duration.fromObject({ months: 6 }).minus({ days: 1 });
 
-logger.debug("Creating tasks, starting on %s", startDate.toISODate());
-logger.debug("Migrating %d days of detailed journal data", detailedSlotTimeWindow.toFormat('d'));
-logger.debug("Migrating %d days of high level journal data", highLevelSlotTimeWindow.toFormat('d'));
-logger.debug("Migrating %d days of deployment data", deploymentTimeWindow.toFormat('d'));
+logger.debug('Creating tasks, starting on %s', startDate.toISODate());
+logger.debug('Migrating %d days of detailed journal data', detailedSlotTimeWindow.toFormat('d'));
+logger.debug('Migrating %d days of high level journal data', highLevelSlotTimeWindow.toFormat('d'));
+logger.debug('Migrating %d days of deployment data', deploymentTimeWindow.toFormat('d'));
 
 createAllTasks();
--- dms-configurator/src/util.ts
+++ dms-configurator/src/util.ts
@@ -1,30 +1,30 @@
 /**
  * Common utility functionality.
  */
-import * as fs from 'fs'
+import * as fs from 'fs';
 import { createLogger, Logger, format, transports } from 'winston';
 
 /**
  * Get a logger to use.
- * 
+ *
  * The logger is for the console, includes timestamps, the module name and coloured log levels.
- * 
+ *
  * @param name      The name of the module
  * @param logLevel  The log level to set (silly, debug, info, warning, error)
  */
 export function getLogger(name: string, logLevel: string): Logger {
-    const logger = createLogger({
-        level: logLevel,
-        format: format.combine(
+  const logger = createLogger({
+    level: logLevel,
+    format: format.combine(
             format.label({ label: name }),
             format.timestamp(),
             format.splat(),
             format.simple()
         ),
-        transports: [ new transports.Console() ]
-    });
+    transports: [new transports.Console()]
+  });
 
-    return logger;
+  return logger;
 }
 
 /**
@@ -32,6 +32,6 @@ export function getLogger(name: string, logLevel: string): Logger {
  * @param name      The file name
  */
 export function loadJSON(name: string): any {
-    const resultText = fs.readFileSync(name);
-    return JSON.parse(resultText.toString('utf8'));
+  const resultText = fs.readFileSync(name);
+  return JSON.parse(resultText.toString('utf8'));
 }
