f90dd8f5e906a694e912dd57492307620b1d1595
Alistair Campbell <Alistair.Campbell@bjss.com>
2019-02-18 10:12:37 +0000

    Removed examiner names from Examiner dataset

--- destination-db/converted-queries.sql
+++ destination-db/converted-queries.sql
@@ -10,14 +10,9 @@ where programme_date between '2017-08-03' and '2017-08-16'
 and examiner_end_date >= '2017-08-03'
 
  -- select examinerDataSet - for a single day
- -- no mysql equiv of initcap(..) for name fields, catered for with user defined function
-select e.individual_id, e.staff_number, title_ref.item_desc1 as title,
-        i.first_forename as first_forename, i.second_forename as second_forename,
-        i.third_forename as third_forename, i.family_name as family_name
-from INDIVIDUAL i
-    join EXAMINER e on e.individual_id = i.individual_id
+select e.individual_id, e.staff_number
+from EXAMINER e
     left join EXAMINER_STATUS es on es.individual_id = e.individual_id
-    left join REF_DATA_ITEM_MASTER title_ref on i.title_code = title_ref.item_id
 where IFNULL(e.grade_code, 'ZZZ') <> 'DELE'
 and IFNULL(es.end_date, '4000-01-01') >= '2017-08-03'
 
