84236a2a7de3c885ee4a0593027ccc0879c945b2
John Morley <john.morley@bjss.com>
2019-02-21 13:44:42 +0000

    Implement config and examiner query for user poller

--- src/common/framework/mysql/database.ts
+++ src/common/framework/mysql/database.ts
@@ -1,11 +1,11 @@
 import * as mysql from 'mysql';
 
 export const query = async (
-  connectionPool: mysql.Pool,
+  connection: mysql.Connection | mysql.Pool,
   sql: string, args?: any,
 ): Promise<any[]>  => {
   return new Promise((resolve, reject) => {
-    connectionPool.query(sql, args, (err, rows) => {
+    connection.query(sql, args, (err, rows) => {
       if (err) {
         reject(err);
       }
--- src/functions/pollUsers/framework/config.ts
+++ src/functions/pollUsers/framework/config.ts
@@ -1,3 +1,44 @@
-export const bootstrapConfig = () => {
+import {
+  defaultIfNotPresent,
+  throwIfNotPresent,
+  tryFetchFromSecretsManager,
+} from '../../../common/framework/config/config-helpers';
+let configuration: Config;
 
+export const bootstrapConfig = async () => {
+  if (!configuration) {
+    configuration = {
+      isOffline: !!process.env.IS_OFFLINE,
+      usersDynamodbTableName: defaultIfNotPresent(process.env.USERS_DDB_TABLE_NAME, 'users'),
+      tarsReplicaDatabaseHostname: throwIfNotPresent(
+        process.env.TARS_REPLICA_HOST_NAME,
+        'tarsReplicateDatabaseHostname',
+      ),
+      tarsReplicaDatabaseName: throwIfNotPresent(
+        process.env.TARS_REPLICA_DB_NAME,
+        'tarsReplicaDatabaseName',
+      ),
+      tarsReplicaDatabaseUsername: throwIfNotPresent(
+        process.env.TARS_REPLICA_DB_USERNAME,
+        'tarsReplicaDatabaseUsername',
+      ),
+      tarsReplicaDatabasePassword: await tryFetchFromSecretsManager(
+        process.env.ASM_SECRET_NAME,
+        process.env.ASM_SECRET_DB_PASSWORD_KEY,
+        'SECRET_DB_PASSWORD_KEY',
+      ),
+
+    };
+  }
 };
+
+export type Config = {
+  isOffline: boolean;
+  usersDynamodbTableName: string;
+  tarsReplicaDatabaseHostname: string;
+  tarsReplicaDatabaseName: string;
+  tarsReplicaDatabaseUsername: string;
+  tarsReplicaDatabasePassword: string;
+};
+
+export const config = (): Config => configuration;
