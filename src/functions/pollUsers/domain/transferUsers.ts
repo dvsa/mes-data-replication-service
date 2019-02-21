b3e75ffcb7a9b3eb16b2267474df477dbdf2556a
John Morley <john.morley@bjss.com>
2019-02-21 11:35:35 +0000

    Implement basic handler with exception handling

---
+++ src/functions/pollUsers/domain/transferUsers.ts
@@ -0,0 +1,3 @@
+export const transferUsers = async () => {
+
+};
--- src/functions/pollUsers/framework/handler.ts
+++ src/functions/pollUsers/framework/handler.ts
@@ -1,7 +1,17 @@
 import { APIGatewayProxyEvent, Context } from 'aws-lambda';
 import createResponse from '../../../common/application/utils/createResponse';
 import Response from '../../../common/application/api/Response';
+import { bootstrapConfig } from '../../pollUsers/framework/config';
+import { transferUsers } from '../domain/transferUsers';
+import { HttpStatus } from '../../../common/application/api/HttpStatus';
 
 export async function handler(event: APIGatewayProxyEvent, fnCtx: Context): Promise<Response> {
-  return createResponse({});
+  try {
+    await bootstrapConfig();
+    await transferUsers();
+    return createResponse({});
+  } catch (error) {
+    console.error(error);
+    return createResponse({}, HttpStatus.INTERNAL_SERVER_ERROR);
+  }
 }
