6dcb11fb6ad43f7c373f676a1c93ef52a80202a0
John Morley <john.morley@bjss.com>
2019-02-21 17:16:44 +0000

    Implement sending journals to Dynamo

--- src/functions/pollUsers/framework/repo/dynamodb/cached-examiner-repository.ts
+++ src/functions/pollUsers/framework/repo/dynamodb/cached-examiner-repository.ts
@@ -1,5 +1,6 @@
 import { DynamoDB } from 'aws-sdk';
 import { config } from '../../../../pollUsers/framework/config';
+import { chunk } from 'lodash';
 
 let dynamoDocumentClient: DynamoDB.DocumentClient;
 const getDynamoClient = () => {
@@ -27,7 +28,28 @@ export const getCachedExaminers = async (): Promise<string[]> => {
 };
 
 export const cacheStaffNumbers = async (staffNumbers: string[]): Promise<void> => {
+  const ddb = getDynamoClient();
+  const tableName = config().usersDynamodbTableName;
+  console.log(`caching ${staffNumbers.length} staff numbers`);
 
+  const maxBatchWriteRequests = 25;
+  const staffNumberWriteBatches = chunk(staffNumbers, maxBatchWriteRequests);
+
+  const writePromises = staffNumberWriteBatches.map((batch) => {
+    const params = {
+      RequestItems: {
+        [tableName]: batch.map(staffNumber => ({
+          PutRequest: {
+            Item: {
+              staffNumber,
+            },
+          },
+        })),
+      },
+    };
+    return ddb.batchWrite(params).promise();
+  });
+  await Promise.all(writePromises);
 };
 
 export const uncacheStaffNumbers = async (staffNumbers: string[]): Promise<void> => {
