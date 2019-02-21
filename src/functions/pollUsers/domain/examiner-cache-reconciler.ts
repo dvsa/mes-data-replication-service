6c13943cca6c69750375dce383cc31aadf97dcb0
John Morley <john.morley@bjss.com>
2019-02-21 14:36:01 +0000

    Implement cache reconciler

--- src/functions/pollUsers/domain/examiner-cache-reconciler.ts
+++ src/functions/pollUsers/domain/examiner-cache-reconciler.ts
@@ -1,3 +1,9 @@
-export const reconcileActiveAndCachedExaminers = async (activeStaffNumbers: string[], cachedStaffNumber: string[]) => {
+import { cacheStaffNumbers, uncacheStaffNumbers } from '../framework/repo/dynamodb/cachedExaminerRepository';
 
+export const reconcileActiveAndCachedExaminers = async (activeStaffNumbers: string[], cachedStaffNumbers: string[]) => {
+  const staffNumbersToCache = activeStaffNumbers.filter(staffNumber => !cachedStaffNumbers.includes(staffNumber));
+  await cacheStaffNumbers(staffNumbersToCache);
+
+  const staffNumbersToUncache = cachedStaffNumbers.filter(staffNumber => !activeStaffNumbers.includes(staffNumber));
+  await uncacheStaffNumbers(staffNumbersToUncache);
 };
--- src/functions/pollUsers/framework/repo/dynamodb/cachedExaminerRepository.ts
+++ src/functions/pollUsers/framework/repo/dynamodb/cachedExaminerRepository.ts
@@ -1,3 +1,11 @@
 export const getCachedExaminers = async (): Promise<string[]> => {
   return Promise.resolve([]);
 };
+
+export const cacheStaffNumbers = async (staffNumbers: string[]): Promise<void> => {
+
+};
+
+export const uncacheStaffNumbers = async (staffNumbers: string[]): Promise<void> => {
+
+};
