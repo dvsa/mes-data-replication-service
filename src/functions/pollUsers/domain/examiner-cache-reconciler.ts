6af49857161a9e52b88e8b805ef10f11eac51f22
John Morley <john.morley@bjss.com>
2019-02-21 16:37:40 +0000

    Add scanning of cached examiners and refactor reconcile for transaction

--- src/functions/pollUsers/domain/examiner-cache-reconciler.ts
+++ src/functions/pollUsers/domain/examiner-cache-reconciler.ts
@@ -1,9 +1,8 @@
-import { cacheStaffNumbers, uncacheStaffNumbers } from '../framework/repo/dynamodb/cached-examiner-repository';
+import { updateStaffNumberCache } from '../framework/repo/dynamodb/cached-examiner-repository';
 
 export const reconcileActiveAndCachedExaminers = async (activeStaffNumbers: string[], cachedStaffNumbers: string[]) => {
   const staffNumbersToCache = activeStaffNumbers.filter(staffNumber => !cachedStaffNumbers.includes(staffNumber));
-  await cacheStaffNumbers(staffNumbersToCache);
-
   const staffNumbersToUncache = cachedStaffNumbers.filter(staffNumber => !activeStaffNumbers.includes(staffNumber));
-  await uncacheStaffNumbers(staffNumbersToUncache);
+
+  await updateStaffNumberCache(staffNumbersToCache, staffNumbersToUncache);
 };
--- src/functions/pollUsers/framework/repo/dynamodb/cached-examiner-repository.ts
+++ src/functions/pollUsers/framework/repo/dynamodb/cached-examiner-repository.ts
@@ -1,11 +1,31 @@
+import { config } from '../../config';
+import { DynamoDB } from 'aws-sdk';
+
+let dynamoDocumentClient: DynamoDB.DocumentClient;
+const getDynamoClient = () => {
+  if (!dynamoDocumentClient) {
+    dynamoDocumentClient = config().isOffline
+      ? new DynamoDB.DocumentClient({ endpoint: 'http://localhost:8000' })
+      : new DynamoDB.DocumentClient();
+  }
+  return dynamoDocumentClient;
+};
+
 export const getCachedExaminers = async (): Promise<string[]> => {
-  return Promise.resolve([]);
+  const ddb = getDynamoClient();
+  const scanParams = {
+    TableName: config().usersDynamodbTableName,
+    ProjectionExpression: 'staffNumber',
+  };
+  const scanResult = await ddb.scan(scanParams).promise();
+
+  if (!scanResult.Items) {
+    return [];
+  }
+
+  return scanResult.Items.map(item => item.staffNumber);
 };
 
-export const cacheStaffNumbers = async (staffNumbers: string[]): Promise<void> => {
-
-};
-
-export const uncacheStaffNumbers = async (staffNumbers: string[]): Promise<void> => {
+export const updateStaffNumberCache = async (cachedStaffNumbers: string[], uncachedStaffNumbers: string[]) => {
 
 };
