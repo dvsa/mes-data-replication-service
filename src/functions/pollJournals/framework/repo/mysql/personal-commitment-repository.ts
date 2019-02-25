affaac797c5b2a54f8bc1ac3ea19ca4d9138a034
John Morley <john.morley@bjss.com>
2019-02-25 12:53:34 +0000

    Next working day calculation for Personal Commitments

--- src/functions/pollJournals/framework/repo/mysql/personal-commitment-repository.ts
+++ src/functions/pollJournals/framework/repo/mysql/personal-commitment-repository.ts
@@ -1,15 +1,10 @@
 import * as mysql from 'mysql';
 import { ExaminerPersonalCommitment } from '../../../domain/examiner-personal-commitment';
-import * as moment from 'moment';
 import { mapRow } from './row-mappers/personal-commitment-row-mapper';
 import { query } from '../../../../../common/framework/mysql/database';
 
 export const getPersonalCommitments = async (connectionPool: mysql.Pool)
   : Promise<ExaminerPersonalCommitment[]> => {
-  const sqlYearFormat = 'YYYY-MM-DD';
-  const windowStart = moment().format(sqlYearFormat);
-  const windowEnd = moment().add(13, 'days').format(sqlYearFormat);
-
   const res = await query(
     connectionPool,
     /* tslint:disable */
@@ -17,20 +12,24 @@ export const getPersonalCommitments = async (connectionPool: mysql.Pool)
     select e.individual_id, pc.commitment_id, pc.start_date_time, pc.end_date_time, pc.non_test_activity_code, reason.reason_desc
     from EXAMINER e 
         join PERSONAL_COMMITMENT pc on e.individual_id = pc.individual_id
-        join NON_TEST_ACTIVITY_REASON reason on pc.non_test_activity_code = reason.non_test_activity_code
+        join NON_TEST_ACTIVITY_REASON reason on pc.non_test_activity_code = reason.non_test_activity_code,
+    (
+      select
+        curdate() as window_start,
+        date_add(curdate(), interval +13 day) as window_end
+    ) windows
     where (
-        DATE(pc.start_date_time) between ? and ?
-        or DATE(pc.end_date_time) between ? and ?
+        DATE(pc.start_date_time) between windows.window_start and windows.window_end
+        or DATE(pc.end_date_time) between windows.window_start and windows.window_end
     )
     and IFNULL(e.grade_code, 'ZZZ') != 'DELE'
     and exists (
         select end_date 
         from EXAMINER_STATUS es
         where es.individual_id = e.individual_id
-        and IFNULL(es.end_date, '4000-01-01') > ?
+        and IFNULL(es.end_date, '4000-01-01') > windows.window_start
     )
     `,
-    [windowStart, windowEnd, windowStart, windowEnd, windowStart]
     /* tslint:enable */
   );
   return res.map(mapRow);
