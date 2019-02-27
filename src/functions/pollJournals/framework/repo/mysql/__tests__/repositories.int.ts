b425c521b1daa76b674c153f907e85aaff94f861
John Morley <41190821+johnmorleybjss@users.noreply.github.com>
2019-02-27 09:54:04 +0000

    MES-1629: Integrate next working day calculation into journal polling (#16)
    * Add `getJournalEndDate` function to the schema
    * Remove fixed window sizes from journal poller queries, replace with window sizes based on next working day
    * Fix issue with DynamoDB local not working due to missing region/AWS config placeholders
    * Add integration tests to Personal Commitment queries

---
+++ src/functions/pollJournals/framework/repo/mysql/__tests__/repositories.int.ts
@@ -0,0 +1,32 @@
+import { personalCommitmentIntegrationTests } from './personal-commitment-repository.int.module';
+import { advanceTestSlotsIntegrationTests } from './advance-test-slot-repository.int.module';
+import * as compose from 'docker-compose';
+const dockerMonitor = require('node-docker-monitor');
+
+jasmine.DEFAULT_TIMEOUT_INTERVAL = 3 * 60 * 1000;
+
+describe('MySQL repository integration tests', () => {
+  beforeAll((done) => {
+    console.log('Starting MySQL...');
+    compose.upAll({ cwd: 'e2e', log: true }).then(() => {
+      dockerMonitor({
+        onContainerUp: () => {},
+        onContainerDown: (container: any) => {
+          if (container.Name === 'mes-data-replication-service-e2e-migration') {
+            console.log('MySQL migration completed');
+            done();
+          }
+        },
+      });
+    });
+  });
+
+  afterAll(() => {
+    return compose.down({ cwd: 'e2e' });
+  });
+
+  describe('MySQL repo suites', () => {
+    personalCommitmentIntegrationTests();
+    advanceTestSlotsIntegrationTests();
+  });
+});
--- src/functions/pollJournals/framework/repo/mysql/personal-commitment-repository.ts
+++ src/functions/pollJournals/framework/repo/mysql/personal-commitment-repository.ts
@@ -1,36 +1,36 @@
 import * as mysql from 'mysql';
 import { ExaminerPersonalCommitment } from '../../../domain/examiner-personal-commitment';
-import * as moment from 'moment';
 import { mapRow } from './row-mappers/personal-commitment-row-mapper';
 import { query } from '../../../../../common/framework/mysql/database';
 
 export const getPersonalCommitments = async (connectionPool: mysql.Pool)
   : Promise<ExaminerPersonalCommitment[]> => {
-  const sqlYearFormat = 'YYYY-MM-DD';
-  const windowStart = moment().format(sqlYearFormat);
-  const windowEnd = moment().add(13, 'days').format(sqlYearFormat);
-
   const res = await query(
     connectionPool,
     /* tslint:disable */
     `
     select e.individual_id, pc.commitment_id, pc.start_date_time, pc.end_date_time, pc.non_test_activity_code, reason.reason_desc
     from EXAMINER e 
-        join PERSONAL_COMMITMENT pc on e.individual_id = pc.individual_id
-        join NON_TEST_ACTIVITY_REASON reason on pc.non_test_activity_code = reason.non_test_activity_code
-    where (
-        DATE(pc.start_date_time) between ? and ?
-        or DATE(pc.end_date_time) between ? and ?
-    )
-    and IFNULL(e.grade_code, 'ZZZ') != 'DELE'
+      join PERSONAL_COMMITMENT pc on e.individual_id = pc.individual_id
+      join NON_TEST_ACTIVITY_REASON reason on pc.non_test_activity_code = reason.non_test_activity_code
+      join
+        (
+          select
+            curdate() as window_start,
+            date_add(date_add(curdate(), interval +14 day), interval -1 second) as window_end
+        ) windows
+        on (
+          DATE(pc.start_date_time) between windows.window_start and windows.window_end
+          or DATE(pc.end_date_time) between windows.window_start and windows.window_end
+        )
+    where IFNULL(e.grade_code, 'ZZZ') != 'DELE'
     and exists (
         select end_date 
         from EXAMINER_STATUS es
         where es.individual_id = e.individual_id
-        and IFNULL(es.end_date, '4000-01-01') > ?
+        and IFNULL(es.end_date, '4000-01-01') > windows.window_start
     )
     `,
-    [windowStart, windowEnd, windowStart, windowEnd, windowStart]
     /* tslint:enable */
   );
   return res.map(mapRow);
