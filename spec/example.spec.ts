deb59f4f4bc1e0a4f5ba5d9ad13b2547c499ea68
Ross Faulds <ross.faulds@bjss.com>
2019-02-28 10:07:31 +0000

    up timeout so integration test completes on slower machines

--- spec/example.spec.ts
+++ spec/example.spec.ts
@@ -1,6 +1,6 @@
 import { startSlsOffline, stopSlsOffline } from './helpers/integration-test-lifecycle';
 
-jasmine.DEFAULT_TIMEOUT_INTERVAL = 30000;
+jasmine.DEFAULT_TIMEOUT_INTERVAL = 60000;
 
 describe('integration test', () => {
   beforeAll((done) => {
