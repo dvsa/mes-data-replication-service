70d5245d212831f832d8f21c3bba8a193baff0d3
John Morley <john.morley@bjss.com>
2019-03-07 11:20:01 +0000

    Remove explicit specification of default journal compression level

--- src/functions/pollJournals/application/journal-compressor.ts
+++ src/functions/pollJournals/application/journal-compressor.ts
@@ -2,5 +2,5 @@ import { ExaminerWorkSchedule } from '../../../common/domain/Schema';
 import { gzipSync } from 'zlib';
 
 export const compressJournal = (journal: ExaminerWorkSchedule): string => {
-  return gzipSync(JSON.stringify(journal), { level: 6 }).toString('base64');
+  return gzipSync(JSON.stringify(journal)).toString('base64');
 };
