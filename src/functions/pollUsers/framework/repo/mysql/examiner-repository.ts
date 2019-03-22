8685fe0c876f8d62fb7d0ad8c5b80b015fbd591d
Jamie Kelly <Jamie.Kelly@bjss.com>
2019-03-22 10:03:33 +0000

    Remove dirty hack for testing with nicer hack

--- src/functions/pollUsers/framework/repo/mysql/examiner-repository.ts
+++ src/functions/pollUsers/framework/repo/mysql/examiner-repository.ts
@@ -11,13 +11,7 @@ export const getActiveExaminers = async (): Promise<string[]> => {
     database: configuration.tarsReplicaDatabaseName,
     user: configuration.tarsReplicaDatabaseUsername,
     password: configuration.tarsReplicaDatabasePassword,
-    /*
-        This is a nasty nasty testing hack as it's the only detection mechanism I can think of that doesn't involve
-        testing for test-specific environment variables. The testing password is < 20 characters, so we can disable
-        the SSL option, whereas the signed password for RDS IAM-based Auth is much larger which enables us to enable
-        the Amazon RDS SSL option.
-    */
-    ssl: configuration.tarsReplicaDatabasePassword.length > 20 ? 'Amazon RDS' : null,
+    ssl: process.env.TESTING_MODE ? null : 'Amazon RDS',
     authSwitchHandler(data, cb) {
       if (data.pluginName === 'mysql_clear_password') {
         cb(null, Buffer.from(`${configuration.tarsReplicaDatabasePassword}\0`));
