1335e7ebf18e3ab1fb034fc893f864d9e097011b
Ross Faulds <ross.faulds@bjss.com>
2019-08-06 10:03:18 +0100

    amend personal commitment to take account of commitment affected slot

--- src/functions/pollJournals/framework/repo/mysql/row-mappers/personal-commitment-row-mapper.ts
+++ src/functions/pollJournals/framework/repo/mysql/row-mappers/personal-commitment-row-mapper.ts
@@ -1,11 +1,9 @@
 import { ExaminerPersonalCommitment } from '../../../../domain/examiner-personal-commitment';
-import { extractDateFromDateTime, extractTimeFromDateTime } from '../../../../application/formatters/date-formatter';
 
 interface PersonalCommitmentRow {
   individual_id: number;
   commitment_id: number;
-  start_date_time: Date;
-  end_date_time: Date;
+  slot_id: number;
   non_test_activity_code: string;
   reason_desc: string;
 }
@@ -15,10 +13,7 @@ export const mapRow = (row: PersonalCommitmentRow): ExaminerPersonalCommitment =
     examinerId: row.individual_id,
     personalCommitment: {
       commitmentId: row.commitment_id,
-      startDate: extractDateFromDateTime(row.start_date_time),
-      startTime: extractTimeFromDateTime(row.start_date_time),
-      endDate: extractDateFromDateTime(row.end_date_time),
-      endTime: extractTimeFromDateTime(row.end_date_time),
+      slotId: row.slot_id,
       activityCode: row.non_test_activity_code,
       activityDescription: row.reason_desc,
     },
