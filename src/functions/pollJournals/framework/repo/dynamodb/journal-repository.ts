07a8fa353475aa9d96f0312b6c45f70e74927c82
Ross Faulds <ross.faulds@bjss.com>
2019-08-16 08:57:17 +0100

    up pollerFrequency to 15 minutes to see extent of performance problem

--- src/functions/pollJournals/framework/repo/dynamodb/journal-repository.ts
+++ src/functions/pollJournals/framework/repo/dynamodb/journal-repository.ts
@@ -23,7 +23,7 @@ const totalSaveDuration = 4 * 1000;
 /*
  * Number of seconds between poller invocations.
  */
-export const pollerFrequency = 180;
+export const pollerFrequency = 900;
 
 export const journalHashesCache = new JournalHashesCache(pollerFrequency);
 let dynamoDocumentClient: DynamoDB.DocumentClient;
