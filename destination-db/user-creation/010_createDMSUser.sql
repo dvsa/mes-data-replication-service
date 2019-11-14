9d56362023965268ddaca18fdd7c0cf7dc19046b
Calvin Junk <c.junk@kainos.com>
2019-11-14 14:42:38 +0000

    MES-4154 - DB User Script

---
+++ destination-db/user-creation/010_createDMSUser.sql
@@ -0,0 +1,5 @@
+CREATE USER 'dms_user' IDENTIFIED WITH mysql_native_password BY '<DMS_USER_PASSWORD>' PASSWORD EXPIRE NEVER;
+
+GRANT ALL ON tarsreplica.* TO 'dms_user';
+
+GRANT ALL ON awsdms_control.* TO 'dms_user';
