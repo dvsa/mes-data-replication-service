8f8da9d8f1254ae4c56b43c836b6faee4e81fc9d
Matt <mculliford.dvsa@icloud.com>
2020-07-21 12:04:44 +0100

    update unit test

--- src/functions/pollJournals/framework/repo/mysql/__tests__/journal-end-date-repository.spec.ts
+++ src/functions/pollJournals/framework/repo/mysql/__tests__/journal-end-date-repository.spec.ts
@@ -21,5 +21,6 @@ describe('getJournalEndDate', () => {
     const expectedEndDate: Date = new Date('2020-01-03');
     const journalEndDate = getJournalEndDate();
     expect(journalEndDate).toEqual(expectedEndDate);
+    delete process.env.FUTURE_JOURNAL_DAYS;
   });
 });
