01f10e122c7eb0054f4e08ce6441c07b52a807da
Ammar Haider <ammar.haider@bjss.com>
2020-10-29 13:24:35 +0000

    amends tests

--- src/functions/pollDelegatedBookings/framework/repo/mysql/__tests__/delegated-examiner-bookings-row-mapper.spec.ts
+++ src/functions/pollDelegatedBookings/framework/repo/mysql/__tests__/delegated-examiner-bookings-row-mapper.spec.ts
@@ -11,18 +11,18 @@ describe('DelegatedBookingsMapper', () => {
   });
   it('should map the first row from the query', () => {
     expect(buildDelegatedBookingsFromQueryResult(mockDelegatedBookingRows)[0])
-      .toEqual(new DelegatedBookingDetail(24306179021, '1234567', bufferStr));
+      .toEqual(new DelegatedBookingDetail(41234567021, '1234567', bufferStr));
   });
   it('should map the second row from the query', () => {
     expect(buildDelegatedBookingsFromQueryResult(mockDelegatedBookingRows)[1])
-      .toEqual(new DelegatedBookingDetail(24306180034, '4583912', bufferStr));
+      .toEqual(new DelegatedBookingDetail(41234568034, '4583912', bufferStr));
   });
   it('should map the third row from the query', () => {
     expect(buildDelegatedBookingsFromQueryResult(mockDelegatedBookingRows)[2])
-      .toEqual(new DelegatedBookingDetail(24306181053, '2468053', bufferStr));
+      .toEqual(new DelegatedBookingDetail(41234569053, '2468053', bufferStr));
   });
   it('should map the fourth row from the query', () => {
     expect(buildDelegatedBookingsFromQueryResult(mockDelegatedBookingRows)[3])
-      .toEqual(new DelegatedBookingDetail(24306182064, '9865321', bufferStr));
+      .toEqual(new DelegatedBookingDetail(41234561064, '9865321', bufferStr));
   });
 });
